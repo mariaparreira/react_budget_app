0e99b1b10f2aa5d4cfd5d5fa53a29effd499f129
mariaparreira <72609612+mariaparreira@users.noreply.github.com>
2023-10-11 09:36:41 +0100

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,17 +2,19 @@ import React, {useContext, useState} from 'react';
 import {AppContext} from '../context/AppContext';
 
 const Budget = () => {
-    const {budget, Currency} = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
+    const {budget, Currency, dispatch} = useContext(AppContext);
 
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+    const handleBudgetChange = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value,
+        })
     }
 
     return (
         <div className = 'alert alert-secondary'>
             <span>Budget: {Currency}</span>
-            <input type = "number" step = "10" value = {newBudget} onChange = {handleBudgetChange}></input>
+            <input type = "number" step = "10" value = {budget} onChange = {event => handleBudgetChange(event.target.value)}></input>
         </div>
     );
 };
